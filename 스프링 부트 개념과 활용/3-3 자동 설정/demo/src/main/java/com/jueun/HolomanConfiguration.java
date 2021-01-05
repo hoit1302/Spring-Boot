bcfc2a1cdf9d8a786d662c56bb8f18eccbc309c1
hoit1302 <jueun000101@gmail.com>
2021-01-05 19:21:27 +0900

    jueun-spring-boot-starter 만들기

---
+++ 스프링 부트 개념과 활용/3-3 자동 설정/demo/src/main/java/com/jueun/HolomanConfiguration.java
@@ -0,0 +1,17 @@
+package com.jueun;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HolomanConfiguration {
+
+    @Bean
+    public Holoman holoman(){
+        Holoman holoman = new Holoman();
+        holoman.setHowLong(5);
+        holoman.setName("jueun");
+        return holoman;
+    }
+
+}
