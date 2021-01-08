f385427719c34ed41d45f283dd634184d6f0050f
hoit1302 <jueun000101@gmail.com>
2021-01-09 00:05:21 +0900

    properties @TEST 해보기

--- 스프링 부트 개념과 활용/4-04 로깅/demo/src/test/java/com/jueun/demo/ApplicationTests.java
+++ 스프링 부트 개념과 활용/4-04 로깅/demo/src/test/java/com/jueun/demo/ApplicationTests.java
@@ -1,13 +1,26 @@
 package com.jueun.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    Environment environment; // 모든 프로퍼티들은 Environment를 통해서 확인할 수 있다, 꼭 스프링에 있는 것을 써야 한다.
+
     @Test
     void contextLoads() {
+        assertThat(environment.getProperty("jueun.name"))
+                .isEqualTo("JUEUN"); // assertThat은 static import 시키기!
     }
 
 }
