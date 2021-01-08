55220f659f860b53ab9045974da1ea783e9854c8
hoit1302 <jueun000101@gmail.com>
2021-01-09 01:02:46 +0900

    귀찮은 코딩 말고 아주 중요한 코딩 @TestPropertySource(locations = "classpath:/test.properties")

--- 스프링 부트 개념과 활용/4-04 로깅/demo/src/test/java/com/jueun/demo/ApplicationTests.java
+++ 스프링 부트 개념과 활용/4-04 로깅/demo/src/test/java/com/jueun/demo/ApplicationTests.java
@@ -11,10 +11,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+// test/resources에 application.properties를 지우고 test.properties을 썼다.
+// 빌드를 할 때 src 밑에 있는 것(src/main/java, src/main/resources)을 빌드를 해서 classpath에 놓고
+// 그 다음에 test 코드를 빌드해서 classpath에 놓는다.
+// 그 때 application.properties와 test 폴더 밑에 있는 test.properties는 파일 명이 다르기 때문에 둘 다 온전히 있다.
+// 그런데 실행한 이 테스트에서 @TestPropertySource(locations = "classpath:/test.properties")로
+// 추가로 test.properties을 사용하라고 한다.
+// 2순위로 우선순위가 높기에 동일한 키값이 있을 때만, 오버라이딩한다.
+// 그래서 main 밑 application.properties 에 있는 JUEUN이 아닌, test.properties에 있는 jueun22가 확인된다.
 @RunWith(SpringRunner.class)
-// @TestPropertySource(properties = "jueun.name=jueun2") // 2순위로, 재정의하면 오버라이딩된다.
+@TestPropertySource(locations = "classpath:/test.properties") // 2순위로, 재정의하면 오버라이딩된다.
 @SpringBootTest
-@SpringBootTest(properties = "jueun.name=jueun3") // 3순위로, 재정의하면 오버라이딩된다.
 class ApplicationTests {
 
     @Autowired
@@ -23,7 +30,7 @@ class ApplicationTests {
     @Test
     void contextLoads() {
         assertThat(environment.getProperty("jueun.name"))
-                .isEqualTo("jueun3"); // assertThat은 static import 시키기!
+                .isEqualTo("jueun22"); // assertThat은 static import 시키기!
     }
 
 }
