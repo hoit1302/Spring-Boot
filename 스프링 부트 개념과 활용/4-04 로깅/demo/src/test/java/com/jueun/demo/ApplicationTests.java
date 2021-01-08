fd0da505479ae678cbecfad6c96836fe0d5f35b6
hoit1302 <jueun000101@gmail.com>
2021-01-09 00:35:35 +0900

    2순위 @TestPropertySource(properties = "jueun.name=jueun2")

--- 스프링 부트 개념과 활용/4-04 로깅/demo/src/test/java/com/jueun/demo/ApplicationTests.java
+++ 스프링 부트 개념과 활용/4-04 로깅/demo/src/test/java/com/jueun/demo/ApplicationTests.java
@@ -6,12 +6,15 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
+@TestPropertySource(properties = "jueun.name=jueun2") // 2순위로, 재정의하면 오버라이딩된다.
 @SpringBootTest
+// @SpringBootTest(properties = "jueun.name=jueun3") // 3순위로, 재정의하면 오버라이딩된다.
 class ApplicationTests {
 
     @Autowired
@@ -20,7 +23,7 @@ class ApplicationTests {
     @Test
     void contextLoads() {
         assertThat(environment.getProperty("jueun.name"))
-                .isEqualTo("hoit1302"); // assertThat은 static import 시키기!
+                .isEqualTo("jueun2"); // assertThat은 static import 시키기!
     }
 
 }
