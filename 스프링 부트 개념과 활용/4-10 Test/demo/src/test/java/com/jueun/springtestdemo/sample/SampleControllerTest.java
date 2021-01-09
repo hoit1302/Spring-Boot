bde9fde75a64be4fb6e73d0edf6934557bb57137
hoit1302 <jueun000101@gmail.com>
2021-01-09 18:36:10 +0900

    test 기본

---
+++ 스프링 부트 개념과 활용/4-10 Test/demo/src/test/java/com/jueun/springtestdemo/sample/SampleControllerTest.java
@@ -0,0 +1,11 @@
+package com.jueun.springtestdemo.sample;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest // (webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+class SampleControllerTest {
+
+}
