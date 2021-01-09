f9856a2f4a61d4c197ceb09390ee44685dec173e
hoit1302 <jueun000101@gmail.com>
2021-01-09 21:50:40 +0900

    @JsonTest

---
+++ 스프링 부트 개념과 활용/4-10 Test/demo/src/test/java/com/jueun/springtestdemo/sample/SampleControllerTest5.java
@@ -0,0 +1,15 @@
+package com.jueun.springtestdemo.sample;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@JsonTest
+class SampleControllerTest5 {
+
+    // @Autowired
+    // JacksonTester<Sample>
+
+}
