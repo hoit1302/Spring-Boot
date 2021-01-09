194b479e7c9abde296fb2127224ca08fa3253122
hoit1302 <jueun000101@gmail.com>
2021-01-09 18:35:53 +0900

    Initialization

---
+++ 스프링 부트 개념과 활용/4-10 Test/demo/src/main/java/com/jueun/springtestdemo/sample/SampleService.java
@@ -0,0 +1,11 @@
+package com.jueun.springtestdemo.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+    public String getName() {
+        return "jueun";
+    }
+}
