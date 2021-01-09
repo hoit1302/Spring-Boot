dd1cc3cc7695955127d5b7249ca1d65e897b7119
hoit1302 <jueun000101@gmail.com>
2021-01-09 18:53:06 +0900

    webEnvironment이 MOCK일 때

--- 스프링 부트 개념과 활용/4-10 Test/demo/src/test/java/com/jueun/springtestdemo/sample/SampleControllerTest.java
+++ 스프링 부트 개념과 활용/4-10 Test/demo/src/test/java/com/jueun/springtestdemo/sample/SampleControllerTest.java
@@ -1,11 +1,31 @@
 package com.jueun.springtestdemo.sample;
 
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest // (webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 class SampleControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception{
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk()) // 200이길 바라고
+                .andExpect(content().string("hello jueun")) // 내용이 이거 이고
+                .andDo(print()); // 요청 온 것을 찍어줬음 좋겠다.
+    }
 }
