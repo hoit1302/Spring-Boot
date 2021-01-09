698c3e36d31b0e1ae0bed1cd089d08251ae73cc0
hoit1302 <jueun000101@gmail.com>
2021-01-09 12:04:35 +0900

    @Bean (Third-party Configuration)

--- 스프링 부트 개념과 활용/4-04 로깅/demo/src/main/java/com/jueun/demo/Application.java
+++ 스프링 부트 개념과 활용/4-04 로깅/demo/src/main/java/com/jueun/demo/Application.java
@@ -3,9 +3,20 @@ package com.jueun.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+
+    // Third-party Configuration
+    // 이 애플리케이션 안에 있는 것이 아니라 JAR 파일 안에 있거나 스프링이 제공하는 프로퍼티클래스는 @Component를 못 붙이니까 @Bean으로.
+    @ConfigurationProperties("server")
+    @Bean
+    public ServerProperties serverProperties(){
+        return new ServerProperties();
+    }
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
