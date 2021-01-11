372ceb31cf75be94ffcfaf496e8b5c2127b98a08
hoit1302 <jueun000101@gmail.com>
2021-01-12 02:20:00 +0900

    Security

---
+++ RESTful Web Services 개발/restful-web-service-master/src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.restfulwebservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+        throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("kenneth")
+                .password("{noop}test1234") // {noop}: encoding 없이 사용할 수 있도록.
+                .roles("USER"); // USER 권한 부여
+
+        // username:kenneth, password:test1234 로 로그인하면 200OK
+    }
+}
