959725a438bb59fe4c4245061b5d4c1dc402b3d9
hoit1302 <jueun000101@gmail.com>
2021-01-13 13:38:40 +0900

    Post 관련( 모르는 것: Optional ,get() , setUser())

--- RESTful Web Services 개발/restful-web-service-master/src/main/java/com/example/restfulwebservice/user/Post.java
+++ RESTful Web Services 개발/restful-web-service-master/src/main/java/com/example/restfulwebservice/user/Post.java
@@ -19,7 +19,9 @@ public class Post {
     private String description;
 
     // User : Post -> 1 : (0~N), Main : Sub -> Parent : Child
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY) // 지연로딩방식
+    //    User Entity 조회 시 매번 Post Entity가 같이 로딩되는 것이 아니라,
+    //    Post data가 로딩되는 시점에 필요한 사용자 데이터를 가지고 오겠다는 뜻이다.
     @JsonIgnore
     private User user;
 }
